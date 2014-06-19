838150e2db4b5c28c73b4d87b6dbc73abdfa0b80
dtompkins <dave@dtompkins.com>
2014-06-19 00:52:47 -0400

    added random generator components

---
+++ random/buildbig.py
@@ -0,0 +1,55 @@
+import html
+import pathlib
+import sys
+
+sys.path.append('../')
+from csvsimple import *
+
+# CONFIGUARATION SETTINGS:
+WWW_OUT_DIR = 'd:/www/teaching/'
+WWW_RAND_DIR = WWW_OUT_DIR + 'random/'
+BIG_FILE = WWW_RAND_DIR + 'responses.csv'
+DATA_DIR = '../data/'
+TEMPLATE_DIR = '../templates/'
+
+#todo: if/when uw changes their evaluations, this will become MUCH more complicated.  KISS for now
+MATCH_TEMPLATE = 'uw-math-1'
+KEYS = ['mc_1', 'mc_2', 'mc_3', 'mc_4', 'mc_5', 'mc_6', 'mc_7', 'mc_8', 'mc_9', 'mc_10', 'mc_11', 'mc_12', 'mc_13',
+        'mc_14', 'mc_15', 'txt_1', 'txt_2', 'txt_3', 'txt_4', 'txt_5', 'txt_6']
+OUT_KEYS = KEYS.copy()
+OUT_KEYS.extend(['resp_id', 'offering', 'path'])
+
+sections = list()
+for f in pathlib.Path(DATA_DIR).glob('*.csv'):
+  if 'responses' not in f.name:
+    sections.append(f.name[:-4])
+
+total_count = 0
+big_list = list()
+
+for section_id in sections:
+  print("processing " + section_id)
+  data = csv_to_dict(DATA_DIR + section_id + '.csv')
+  if data['template'] == MATCH_TEMPLATE:
+    data.update(csv_to_dict(TEMPLATE_DIR + data['course'] + '.csv'))
+    offering = data['term'] + ' - ' + data['coursecode'] + ' - ' + data['section']
+    path = '../' + section_id + '.html'
+    responses = SimpleCSV(DATA_DIR + section_id + '-responses.csv')
+    for resp in responses:
+      total_count += 1
+      resp['resp_id'] = total_count
+      resp['offering'] = offering
+      resp['path'] = path
+      del resp['mc_16']
+      for k in resp:
+        if k[:3] == 'txt':
+          resp[k] = html.escape(resp[k]).replace('[nl]', '<br />')
+      big_list.append(resp)
+
+with open(BIG_FILE, 'w', newline='', encoding='utf-8') as f:
+  writer = csv.DictWriter(f, OUT_KEYS)
+  writer.writeheader()
+  writer.writerows(big_list)
+
+print("DON'T FORGET TO UPDATE total_count in geteval.py")
+print("total_count = {}".format(total_count))
---
+++ random/geteval.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import csv
+import json
+import random
+
+print("Content-type: application/json")
+print()
+
+#todo: make this more robust.  currently I only have to change it once a semester
+total_count = 777
+random.seed()
+resp_id = random.randrange(total_count) + 1
+
+#todo: currently, just reading in all of the file is "fast enough"
+with open('responses.csv', 'r') as f:
+  responses = csv.DictReader(f)
+  count = 0
+  for r in responses:
+    count += 1
+    if count == resp_id:
+      print(json.dumps(r))
+      break
